fb65f3d92e639e4fc94ed260ac8039bab6fe305e
jdinh11 <jaden.dinh02@gmail.com>
2023-11-13 20:27:32 -0500

    Update gui.py
    Changed from tkinter to pygame

--- gui.py
+++ gui.py
@@ -1,6 +1,16 @@
-import tkinter as tk
+import pygame 
 
-class GUI(tk.Frame):
+DIMENSION = 800
 
-    def __init__(self):
-        pass
+pygame.init()
+screen = pygame.display.set_mode((DIMENSION, DIMENSION))
+running = True
+
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+pygame.quit()
